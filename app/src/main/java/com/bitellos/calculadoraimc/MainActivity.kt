e5c2eb447568c12ca5d23be0bfeed70624f73f62
Bitello <bitello@outlook.com>
2024-01-31 17:09:17 -0300

    Start

--- app/src/main/java/com/bitellos/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/bitellos/calculadoraimc/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.bitellos.calculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular_IMC)
+
+        btnCalcular.setOnClickListener {
+            val intent = Intent(this, ResultadoActivity::class.java)
+
+            startActivity( intent)
+        }
+
     }
+
 }
